5adda2e0f74ffa2056052e53af791fbef86c1065
Jollo3333 <yilinl8@yw.edu>
2021-03-13 15:34:38 -0500

    integrating question 4 / question 2 solutions, adding question 4/question 2 testing into testing file

--- Testing.py
+++ Testing.py
@@ -1,11 +1,13 @@
 from cse163_utils import assert_equals
+import analysis
 import pandas as pd
 
 
 def test_fastest_growing_job(data):
     """
-    test the data set for the job with the highest
-    Job Outlook rate 2019-29
+    The function test the data set for the job with the highest
+    Job Outlook rate 2019-29, it indicates the highest predicting
+    rate of occupation growing from 2019 to 2029
     """
 
     assert_equals(61.0, data['Job Outlook, 2019-29'].max())
@@ -13,8 +15,9 @@ def test_fastest_growing_job(data):
 
 def test_employment_declining(data):
     """
-    test the data set for the job that has the highest number
-    of loss 2019-29
+    The function test the data set for the job that has the highest number
+    of loss 2019-29. It takes the dataset's Employment change column
+    and calculate the higest number of lost job from 2019 to 2029
     """
 
     assert_equals(-327400.0, data['Employment Change, 2019-29'].min())
@@ -22,29 +25,71 @@ def test_employment_declining(data):
 
 def test_employment_growing(data):
     """
-    test the data set for the job that has the highest number
-    of growth 2019-29
+    The function test the data set for the job that has the highest number
+    of growth 2019-29. It takes the dataset's Employment change column
+    and calculate the higest number of new job from 2019 to 2029
     """
 
     assert_equals(1159500.0, data['Employment Change, 2019-29'].max())
 
 
+def test_clean_data(data, data2):
+    """
+    This function dictates if plugging
+    in the given values will give a certain
+    determined result to tell if the clean_data
+    function in Research_Question_4.py works as expected.
+    Should all tests pass, it will print nothing.
+    On the other hand, if one of these tests fails,
+    it will show an error message.
+    """
+    expect_major = ['Physics',
+                    'Computer Science & Engineering', 'Applied Mathematics']
+    expect_pay = [62300, 74000, 64400]
+    expect_2019_pay = [107510, 92030, 122220]
+    assert_equals(expect_major, data['Major'].tolist())
+    assert_equals(expect_pay, data['Early Career Pay'].tolist())
+    assert_equals(expect_2019_pay, data2['2019 Median Pay'].tolist())
+
+
+def test_uw(data):
+    """
+    This function dictates if plugging
+    in the given values will give a certain
+    determined result to tell if the uw
+    function in Research_Question_4.py works as expected.
+    Should all tests pass, it will print nothing.
+    On the other hand, if one of these tests fails,
+    it will show an error message.
+    """
+    expect_uw_majors = ['Physics', 'Applied Mathematics']
+    given = analysis.uw('UW Majors.txt', data)
+    assert_equals(expect_uw_majors, given['Major'].tolist())
+
+
 def main():
-    data = pd.read_csv('./occupation_data.csv')
+    # Question 1 testing
+    data_question_1 = pd.read_csv('./occupation_data.csv')
 
     # pre data wrangling for the testing, delete all strings
     # in Job Outlook, 2019-29 column
-    data['Job Outlook, 2019-29'] = data['Job Outlook, '
+    data_question_1['Job Outlook, 2019-29'] = data_question_1['Job Outlook, '
                                         '2019-29'].str.replace(r' \(.*\)', '')
-
-    data['Job Outlook, 2019-29'] = data['Job Outlook, '
+    data_question_1['Job Outlook, 2019-29'] = data_question_1['Job Outlook, '
                                         '2019-29'].str.replace('%', '')
+    data_question_1['Job Outlook, 2019-29'] = pd.to_numeric(data_question_1['Job Outlook, 2019-29'])
 
-    data['Job Outlook, 2019-29'] = pd.to_numeric(data['Job Outlook, 2019-29'])
+    test_fastest_growing_job(data_question_1)
+    test_employment_declining(data_question_1)
+    test_employment_growing(data_question_1)
 
-    test_fastest_growing_job(data)
-    test_employment_declining(data)
-    test_employment_growing(data)
+    # Question 2/4 testing
+    data_question_4 = pd.read_csv('Payscale Test.csv', encoding='cp1252')
+    data_question_2 = pd.read_csv('occupation Test.csv', encoding='cp1252')
+    cleaned = analysis.clean_data_1(data_question_4)
+    cleaned2 = analysis.clean_data_2(data_question_2)
+    test_clean_data(cleaned, cleaned2)
+    test_uw(cleaned)
 
 
 if __name__ == '__main__':
--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -87,13 +88,241 @@ def plot_job_outlook(data):
     plt.savefig('Job_Outlook_growth_plot')
 
 
+# Question 4
+
+def clean_data_1(data):
+    """
+    This method takes in the Payscale
+    dataset to clear out any unnessary
+    phrases in each cell of the required
+    columns for further analysis. It also
+    ranks the Early Career Pay and Mid Career
+    Pay columns and sums up their scores
+    together. It returns a modified version of
+    the original dataset with ranked scores of
+    the Early Career Pay, the Mid Career Pay, and
+    the sum of these two scores in three new columns.
+    """
+    data['Major'] = data['Major'].str.replace('Major:', '')
+    data['Major'] = data['Major'].str.replace(r' \(.*\)', '')
+    data['Early Career Pay'] = data['Early Career Pay'].map(
+                                                lambda x: x.lstrip(
+                                                    'Early Career Pay:$'))
+    data['Early Career Pay'] = pd.to_numeric(data['Early Career Pay'])
+    data['Mid-Career Pay'] = data['Mid-Career Pay'].map(
+                                                lambda x: x.lstrip(
+                                                    'Mid-Career Pay:$'))
+    data['Mid-Career Pay'] = pd.to_numeric(data['Mid-Career Pay'])
+    data['Early Career Pay Rank'] = data['Early Career Pay'].rank(method='max')
+    data['Mid-Career Pay Rank'] = data['Mid-Career Pay'].rank(method='max')
+    sum_rankings = data['Early Career Pay Rank'] + data['Mid-Career Pay Rank']
+    data['Resultant Score'] = sum_rankings
+    return data
+
+
+def uw(file_name, data):
+    """
+    This method takes in the cleaned Payscale
+    dataset and the file dataset of every
+    known UW major offered and checks to
+    see if the Payscale dataset contains each
+    major found in the file. It returns a modified
+    version of the cleaned Payscale dataset that
+    details the the majors found at UW and the
+    information about them.
+    """
+    uw_majors_list = []
+    with open(file_name) as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.strip()
+            uw_majors_list.append(line)
+    uw_majors = data['Major'].isin(uw_majors_list)
+    uw_majors.name = 'At UW?'
+    data = pd.concat([data, uw_majors], axis=1)
+    data = data[data['At UW?']]
+    return data
+
+
+def top_10(data):
+    """
+    This method takes in the Payscale
+    dataset that contains information
+    about each major offered at UW and
+    creates a clustered bar plot that
+    details the ten majors with the highest
+    Early and Mid-Career Salaries.
+    """
+    top_10 = data.sort_values(by=['Resultant Score'], ascending=False).head(10)
+    top_10_e = top_10['Early Career Pay'].tolist()
+    top_10_m = top_10['Mid-Career Pay'].tolist()
+    top_10_labels = top_10['Major'].tolist()
+    x = np.arange(len(top_10_labels))
+    width = 0.35
+    fig, ax = plt.subplots()
+    ax.bar(x - width/2, top_10_e, width, label='Early')
+    ax.bar(x + width/2, top_10_m, width, label='Mid')
+    ax.set_ylabel('Pay Amount')
+    ax.set_title('Top 10 Highest Paying Majors At UW')
+    ax.set_xticks(x)
+    ax.set_xticklabels(top_10_labels)
+    plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
+    plt.xticks(rotation=-90)
+    plt.show()
+
+
+def bottom_10(data):
+    """
+    This method takes in the Payscale
+    dataset that contains information
+    about each major offered at UW and
+    creates a clustered bar plot that
+    details the ten majors with the lowest
+    Early and Mid-Career Salaries.
+    """
+    bottom_10 = data.sort_values(by=['Resultant Score']).head(10)
+    bottom_10_e = bottom_10['Early Career Pay'].tolist()
+    bottom_10_m = bottom_10['Mid-Career Pay'].tolist()
+    bottom_10_labels = bottom_10['Major'].tolist()
+    x = np.arange(len(bottom_10_labels))
+    width = 0.35
+    fig, ax = plt.subplots()
+    ax.bar(x - width/2, bottom_10_e, width, label='Early')
+    ax.bar(x + width/2, bottom_10_m, width, label='Mid')
+    ax.set_ylabel('Pay Amount')
+    ax.set_title('Top 10 Lowest Paying Majors At UW')
+    ax.set_xticks(x)
+    ax.set_xticklabels(bottom_10_labels)
+    ax.legend()
+    plt.xticks(rotation=-90)
+    plt.show()
+
+# Question 2
+
+def clean_data_2(data):
+    """
+    This method takes in the occupation
+    dataset to clear out any unnessary
+    phrases in each cell of the required
+    columns for further analysis. It returns a
+    modified version of the original dataset with
+    all cells in the 2019 Median Pay column
+    all turned into integers for the purpose of
+    analyzing the pay amount salary.
+    """
+    data = data.dropna()
+    pay = data['2019 Median Pay']
+    data['2019 Median Pay'] = pay.str.replace('$', '')
+    data['2019 Median Pay'] = pay.str.replace(',', '')
+    data['2019 Median Pay'] = pay.str.extract(r'(\d+)', expand=False)
+    data['2019 Median Pay'] = pd.to_numeric(pay)
+    return data
+
+
+def bach_10(data):
+    """
+    This method takes in the cleaned occupation
+    dataset that contains information
+    about the 2019 Median Pay for jobs that
+    require a Bachelor's degree for entry level
+    and creates a bar plot showing the top 10
+    highest paying occupations for the Bachelor's
+    degree and their respective salaries.
+    """
+    data = data[data['Typical Entry-Level Education'] == 'Bachelor\'s degree']
+    highest_paying_bach = data.sort_values(by=['2019 Median Pay'],
+                                           ascending=False).head(10)
+    highest_paying_bach = highest_paying_bach.head(10)
+    bach_10 = highest_paying_bach['2019 Median Pay'].tolist()
+    bach_10_labels = highest_paying_bach['occupation_name'].tolist()
+    x = np.arange(len(bach_10_labels))
+    width = 0.35
+    fig, ax = plt.subplots()
+    ax.bar(x, bach_10, width)
+    ax.set_ylabel('Mean Annual Salary')
+    ax.set_title('Highest Paying Bachelor\'s Degree Occupations')
+    ax.set_xticks(x)
+    ax.set_xticklabels(bach_10_labels)
+    plt.xticks(rotation=-90)
+    plt.show()
+
+
+def master_10(data):
+    """
+    This method takes in the cleaned occupation
+    dataset that contains information
+    about the 2019 Median Pay for jobs that
+    require a Master's degree for entry level
+    and creates a bar plot showing the top 10
+    highest paying occupations for the Master's
+    degree and their respective salaries.
+    """
+    data = data[data['Typical Entry-Level Education'] == 'Master\'s degree']
+    highest_paying_master = data.sort_values(by=['2019 Median Pay'],
+                                             ascending=False).head(10)
+    highest_paying_master = highest_paying_master.head(10)
+    master_10 = highest_paying_master['2019 Median Pay'].tolist()
+    master_10_labels = highest_paying_master['occupation_name'].tolist()
+    x = np.arange(len(master_10_labels))
+    width = 0.35
+    fig, ax = plt.subplots()
+    ax.bar(x, master_10, width)
+    ax.set_ylabel('Mean Annual Salary')
+    ax.set_title('Highest Paying Master\'s Degree Occupations')
+    ax.set_xticks(x)
+    ax.set_xticklabels(master_10_labels)
+    plt.xticks(rotation=-90)
+    plt.show()
+
+
+def doct_10(data):
+    """
+    This method takes in the cleaned occupation
+    dataset that contains information
+    about the 2019 Median Pay for jobs that
+    require a Doctorate or Professional degree for
+    entry level and creates a bar plot showing the top 10
+    highest paying occupations for the Doctorate and
+    Professional degree and their respective salaries.
+    """
+    data = data[data['Typical Entry-Level Education']
+                == 'Doctoral or professional degree']
+    highest_paying_doct = data.sort_values(by=['2019 Median Pay'],
+                                           ascending=False).head(10)
+    highest_paying_doct = highest_paying_doct.head(10)
+    doct_10 = highest_paying_doct['2019 Median Pay'].tolist()
+    doct_10_labels = highest_paying_doct['occupation_name'].tolist()
+    x = np.arange(len(doct_10_labels))
+    width = 0.35
+    fig, ax = plt.subplots()
+    ax.bar(x, doct_10, width)
+    ax.set_ylabel('Mean Annual Salary')
+    ax.set_title('Highest Paying Doctorate or Professional Degree Occupations')
+    ax.set_xticks(x)
+    ax.set_xticklabels(doct_10_labels)
+    plt.xticks(rotation=-90)
+    plt.show()
+
+
+
 def main():
-    data = pd.read_csv('./occupation_data.csv')
+    data_question_1 = pd.read_csv('./occupation_data.csv')
 
-    plot_employment_decline(data)
-    plot_employment_growth(data)
+    plot_employment_decline(data_question_1)
+    plot_employment_growth(data_question_1)
+    plot_job_outlook(data_question_1)
 
-    plot_job_outlook(data)
+    data_question_4 = pd.read_csv('Final Project Dataset.csv', encoding='cp1252')
+    data_question_4 = clean_data_1(data_question_4)
+    data_question_4 = uw('UW Majors.txt', data_question_4)
+    top_10(data_question_4)
+    bottom_10(data_question_4)
+
+    data_question_2 = pd.read_csv('occupation_data.csv', encoding='cp1252')
+    data_question_2 = clean_data_2(data_question_2)
+    bach_10(data_question_2)
+    master_10(data_question_2)
+    doct_10(data_question_2)
 
 
 if __name__ == '__main__':
